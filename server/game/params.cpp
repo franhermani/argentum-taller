a1aa434d57e64444d984ede02d1f1d66b69443ca
franhermani <franhermani@gmail.com>
2020-06-08 22:36:57 -0300

    JSON filled with all game params

--- server/game/params.cpp
+++ server/game/params.cpp
@@ -1,5 +1,3 @@
 #include "params.h"
 
-GameParams::GameParams(const json &json_params) : jsonParams(json_params),
-worldWidth(json_params["world_width"]),
-worldHeight(json_params["world_height"]) {}
+GameParams::GameParams(const json& json_params) : jsonParams(json_params) {}
