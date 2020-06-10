935daf0d51b9c5f9218df818ad496b43e7872bff
franhermani <franhermani@gmail.com>
2020-06-10 19:03:49 -0300

    Game loop first version

--- server/game/manager.cpp
+++ server/game/manager.cpp
@@ -1,4 +1,8 @@
+#include <thread>
+#include <chrono>
 #include "manager.h"
+
+#define MS_PER_UPDATE 60
 
 GameManager::GameManager(FileParser& file_parser) : fileParser(file_parser),
 params(fileParser.getGameParams()) {
@@ -7,8 +11,20 @@ params(fileParser.getGameParams()) {
 }
 
 void GameManager::run() {
+    using clock = std::chrono::system_clock;
+    using ms = std::chrono::milliseconds;
+
     while (keepRunning) {
-        // TODO: ...
+        auto start = clock::now();
+
+        // TODO:
+        // - Saco un evento de la cola
+        // - Llamo al handler del evento
+
+        auto end = clock::now();
+        auto elapsed = std::chrono::duration_cast<ms>(end - start).count();
+        auto time_to_sleep = MS_PER_UPDATE - elapsed;
+        if (time_to_sleep > 0) std::this_thread::sleep_for(ms(time_to_sleep));
     }
     isRunning = false;
 }
