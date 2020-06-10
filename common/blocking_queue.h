a7de4a723d0a6987a76938b653a5b1601aab884d
Martin <martin.rosas@despegar.com>
2020-06-10 20:38:47 -0300

    added template blocking queue

---
+++ common/blocking_queue.h
@@ -0,0 +1,31 @@
+#ifndef TP2_BLOCKINGQUEUE_H
+#define TP2_BLOCKINGQUEUE_H
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+
+
+class ClosedQueueException : public std::exception {
+public:
+    const char* what() {
+        return "Queue is closed";
+    }
+};
+
+template <class T> class BlockingQueue {
+private:
+    std::mutex m;
+    std::queue<T> queue;
+    std::condition_variable cv;
+public:
+    bool isClosed;
+    BlockingQueue();
+    void push(T t);
+    T pop();
+    void close();
+    ~BlockingQueue();
+};
+
+
+#endif //TP2_BLOCKINGQUEUE_H
