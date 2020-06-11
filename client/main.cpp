7354db72e72e6e3af9688b0667cb1bdd7ed2b5a0
franhermani <franhermani@gmail.com>
2020-06-10 22:54:06 -0300

    Deleted glade usage from project

--- client/main.cpp
+++ client/main.cpp
@@ -1,4 +1,3 @@
-#include <gtkmm.h>
 #include <iostream>
 #include "communication/client.h"
 #include "../common/socket_error.h"
@@ -32,39 +31,3 @@ int main(int argc, char *argv[]) {
     }
     return OK;
 }
-
-/*
-int main(int argc, char *argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-
-    Gtk::Dialog* pDialog;
-
-    // Cargo el GtkBuilder file e instancio sus widgets
-    auto refBuilder = Gtk::Builder::create();
-
-    try {
-        // TODO: no encuentra el archivo
-        refBuilder->add_from_file("basic.glade");
-    } catch(const Glib::FileError& e) {
-        std::cerr << "FileError: " << e.what() << std::endl;
-        return ERROR;
-    } catch(const Glib::MarkupError& e) {
-        std::cerr << "MarkupError: " << e.what() << std::endl;
-        return ERROR;
-    } catch(const Gtk::BuilderError& e) {
-        std::cerr << "BuilderError: " << e.what() << std::endl;
-        return ERROR;
-    }
-
-    // Obtengo el GtkBuilder widget
-    refBuilder->get_widget("DialogBasic", pDialog);
-
-    // Ejecuto el dialogo
-    if(pDialog) app->run(*pDialog);
-
-    // Elimino el dialogo
-    delete pDialog;
-
-    return OK;
-}
-*/
