5cb52d4c4ab6cbbbe6b0ca178c652deb2041da69
franhermani <franhermani@gmail.com>
2020-06-14 16:15:49 -0300

    Serialize tested and working OK

--- common/commands/move_command.cpp
+++ common/commands/move_command.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <cstring>
 #include <netinet/in.h>
 #include "move_command.h"
 #include "defines.h"
@@ -7,23 +6,24 @@
 MoveCommand::MoveCommand(const int direction) : direction(direction) {}
 
 const std::vector<char> MoveCommand::serialize() const {
+    // Longitud de los argumentos
+    uint8_t arguments_size = sizeof(uint8_t);
+
     // Longitud total
-    uint8_t arguments_size = sizeof(moveDirection);
-    int total_size = sizeof(commandType) + sizeof(uint16_t) + arguments_size;
+    int total_size = sizeof(uint8_t) + sizeof(uint8_t) + arguments_size;
 
     // Vector serializado
     std::vector<char> byte_msg;
     byte_msg.resize(total_size);
 
     // Tipo de comando
-    byte_msg.push_back(CMD_MOVE);
+    byte_msg[0] = CMD_MOVE;
 
     // Longitud de los argumentos
-    uint16_t length = htons(arguments_size);
-    memcpy(&byte_msg.back(), &length, sizeof(length));
+    byte_msg[1] = arguments_size;
 
     // Argumentos
-    byte_msg.push_back(direction);
+    byte_msg[2] = direction;
 
     return byte_msg;
 }
