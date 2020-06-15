8f1a4dae98464fdfb99978b2202af5c0d5b5b1f5
Martin <martin.rosas@despegar.com>
2020-06-15 14:35:13 -0300

    created gamerender script

---
+++ client/communication/game_render.h
@@ -0,0 +1,24 @@
+
+#ifndef ARGENTUM_GAME_RENDER_H
+#define ARGENTUM_GAME_RENDER_H
+#include <string>
+#include <vector>
+#include "connection_handler.h"
+#include <SDL2/SDL_image.h>
+
+
+class GameRender {
+private:
+    const int screen_width;
+    const int screen_height;
+    const int blocks_width;
+    const int blocks_height;
+public:
+    GameRender(const int screen_width, const int screen_height, const int blocks_width, const int blocks_height);
+    ~GameRender();
+    int init();
+    void play();
+};
+
+
+#endif //ARGENTUM_GAME_RENDER_H
