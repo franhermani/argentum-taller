e99fac144f833e5e5b2c321775ed90352206f5b6
franhermani <franhermani@gmail.com>
2020-06-15 22:23:03 -0300

    Minor fixes

--- server/communication/client_handler.h
+++ server/communication/client_handler.h
@@ -10,10 +10,10 @@
 
 class ClientHandler : public Thread {
     Socket socket;
-    Player* player;
-    ClientSender* clientSender;
-    ClientReceiver* clientReceiver;
     GameManager& gameManager;
+    ClientReceiver* clientReceiver;
+    ClientSender* clientSender;
+    Player* player{};
 
 public:
     // Constructor
