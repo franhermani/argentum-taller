1a59d509ac125bd1ef8b5600cf281919d9aa4091
franhermani <franhermani@gmail.com>
2020-06-19 17:24:29 -0300

    Sending matrix OK

--- server/communication/protocol.cpp
+++ server/communication/protocol.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include "protocol.h"
+#include "../../common/defines/world_structs.h"
 #include "../../common/defines/debug.h"
 
 #define BYTE_SIZE 1
@@ -42,6 +43,39 @@ Command* ServerProtocol::receiveCommand(Player& player) {
         socket.receiveBytes(arguments.data(), arguments.size());
     }
     return commandFactory(player, type, arguments);
+}
+
+void ServerProtocol::sendMatrix(WorldMonitor &world_monitor) {
+    std::vector<std::vector<Terrain>> matrix = world_monitor.getMatrix();
+    int width = world_monitor.getWidth();
+    int height = world_monitor.getHeight();
+    int matrix_length = width * height;
+
+    matrix_t m;
+    m.length = htons(2*sizeof(uint16_t) + matrix_length*sizeof(uint8_t));
+    m.width = htons(width);
+    m.height = htons(height);
+
+    std::vector<char> byte_msg;
+    byte_msg.resize(3*sizeof(uint16_t) + matrix_length);
+    memcpy(&byte_msg[0], &m.length, sizeof(uint16_t));
+    memcpy(&byte_msg[2], &m.width, sizeof(uint16_t));
+    memcpy(&byte_msg[4], &m.height, sizeof(uint16_t));
+
+    int i, j, k = 0;
+    for (i = 0; i < height; i ++) {
+        for (j = 0; j < width; j ++) {
+            byte_msg[6+(k++)] = matrix[i][j];
+        }
+    }
+    socket.sendBytes(byte_msg.data(), byte_msg.size());
+
+    if (debug) {
+        std::cout << "Matriz enviada: ";
+        for (char& c : byte_msg)
+            printf("%02X ", (unsigned) (unsigned char) c);
+        std::cout << "\n";
+    }
 }
 
 void ServerProtocol::sendWorldAround(WorldMonitor& world_monitor,
