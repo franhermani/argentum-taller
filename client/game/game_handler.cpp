db8fa88dcb93f20f7ed6a986771d5631b1384f6a
franhermani <franhermani@gmail.com>
2020-06-22 00:08:07 -0300

    Deleted thread sleep

--- client/game/game_handler.cpp
+++ client/game/game_handler.cpp
@@ -2,21 +2,17 @@
 #include <string>
 #include <vector>
 #include <zconf.h>
-#include <chrono>
 #include "game_handler.h"
 #include "../../common/defines/username_confirmation.h"
-
-#define SLEEP_TIME 3000
 
 GameHandler::GameHandler(const char *host, const char *port,
         const std::string& username, const uint8_t race_type,
         const uint8_t class_type) : socket(host, port, false),
         gameRender(640*2, 480*2) {
     connectionSender = new ConnectionSender(socket, commandQueue);
-    connectionReceiver = new ConnectionReceiver(socket, gameRender);
-
     connectionSender->sendPlayerInfo(username, race_type, class_type);
 
+    connectionReceiver = new ConnectionReceiver(socket, gameRender);
     int code = connectionReceiver->receiveUsernameConfirmation();
     if (code != USERNAME_OK) {
         delete connectionSender;
@@ -55,10 +51,6 @@ void GameHandler::stop() {
 }
 
 void GameHandler::printStartMessage() {
-    using ms = std::chrono::milliseconds;
-
     std::cout << "\n¡Jugador creado correctamente!\n" <<
     "El juego comenzará en unos instantes...\n";
-
-    std::this_thread::sleep_for(ms(SLEEP_TIME));
 }
