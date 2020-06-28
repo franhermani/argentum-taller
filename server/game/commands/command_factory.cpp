fc8c94376a53e38e57529feb1595e0a3e262767a
franhermani <franhermani@gmail.com>
2020-06-28 13:34:08 -0300

    Added TakeCommand to factory

--- server/game/commands/command_factory.cpp
+++ server/game/commands/command_factory.cpp
@@ -8,8 +8,12 @@
 #include "revive_command.h"
 #include "heal_command.h"
 #include "list_command.h"
+#include "take_command.h"
 #include "move_command.h"
 #include "attack_command.h"
+
+#define SIZE_8      sizeof(uint8_t)
+#define SIZE_16     sizeof(uint16_t)
 
 CommandFactory::CommandFactory() = default;
 
@@ -17,6 +21,7 @@ Command* CommandFactory::operator()(Player& player, int type,
         std::vector<char>& arguments) {
     if (type == CMD_MEDITATE) {
         return new MeditateCommand(player);
+
     } else if (type == CMD_REVIVE) {
         if (! arguments.empty()) {
             uint16_t priest_id;
@@ -25,40 +30,58 @@ Command* CommandFactory::operator()(Player& player, int type,
         } else {
             return new ReviveCommand(player);
         }
+
     } else if (type == CMD_HEAL) {
         uint16_t priest_id;
         memcpy(&priest_id, arguments.data(), arguments.size());
         return new HealCommand(player, ntohs(priest_id));
+
     } else if (type == CMD_DEPOSIT) {
         // TODO:...
+
     } else if (type == CMD_WITHDRAW) {
         // TODO:...
+
     } else if (type == CMD_DEPOSIT_GOLD) {
         // TODO:...
+
     } else if (type == CMD_WITHDRAW_GOLD) {
         // TODO:...
+
     } else if (type == CMD_LIST) {
         uint16_t npc_id;
         memcpy(&npc_id, arguments.data(), arguments.size());
         return new ListCommand(player, ntohs(npc_id));
+
     } else if (type == CMD_BUY) {
         // TODO:...
+
     } else if (type == CMD_SELL) {
         // TODO:...
+
     } else if (type == CMD_TAKE) {
-        // TODO:...
+        uint16_t pos_x, pos_y;
+        memcpy(&pos_x, arguments.data(), SIZE_16);
+        memcpy(&pos_y, arguments.data() + SIZE_16, SIZE_16);
+        return new TakeCommand(player, ntohs(pos_x), ntohs(pos_y));
+
     } else if (type == CMD_THROW) {
         // TODO:...
+
     } else if (type == CMD_MOVE) {
         int direction = arguments[0];
         return new MoveCommand(player, direction);
+
     } else if (type == CMD_ATTACK) {
         uint8_t enemy_id_type = arguments[0];
         uint16_t enemy_id;
-        memcpy(&enemy_id, arguments.data() + 1, arguments.size() - 1);
+        memcpy(&enemy_id, arguments.data() + SIZE_8,
+                arguments.size() - SIZE_8);
         return new AttackCommand(player, enemy_id_type, ntohs(enemy_id));
+
     } else if (type == CMD_EQUIP) {
         // TODO:...
+
     } else if (type == CMD_TRICK) {
         // TODO:...
     }
