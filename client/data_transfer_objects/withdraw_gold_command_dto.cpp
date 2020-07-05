1ca1c2b5273b0a46c241a2b8bff253b21f84278e
franhermani <franhermani@gmail.com>
2020-07-05 13:47:09 -0300

    Added withdraw gold command dto

--- client/data_transfer_objects/withdraw_gold_command_dto.cpp
+++ client/data_transfer_objects/withdraw_gold_command_dto.cpp
@@ -1,22 +1,22 @@
 #include <vector>
 #include <cstring>
 #include <arpa/inet.h>
-#include "deposit_item_command_dto.h"
+#include "withdraw_gold_command_dto.h"
 #include "../../common/defines/commands.h"
 
 #define SIZE_8      sizeof(uint8_t)
 #define SIZE_16     sizeof(uint16_t)
 
-DepositItemCommandDTO::DepositItemCommandDTO(const uint8_t item_type,
+WithdrawGoldCommandDTO::WithdrawGoldCommandDTO(const uint16_t quantity,
         const uint16_t banker_pos_x, const uint16_t banker_pos_y) :
-        itemType(item_type), bankerPosX(banker_pos_x),
-        bankerPosY(banker_pos_y) {}
+quantity(quantity), bankerPosX(banker_pos_x),
+bankerPosY(banker_pos_y) {}
 
-DepositItemCommandDTO::~DepositItemCommandDTO() = default;
+WithdrawGoldCommandDTO::~WithdrawGoldCommandDTO() = default;
 
-const std::vector<char> DepositItemCommandDTO::serialize() const {
+const std::vector<char> WithdrawGoldCommandDTO::serialize() const {
     // Longitud de los argumentos
-    uint8_t arguments_size = sizeof(itemType) + sizeof(bankerPosX) +
+    uint8_t arguments_size = sizeof(quantity) + sizeof(bankerPosX) +
                              sizeof(bankerPosY);
 
     // Longitud total
@@ -27,18 +27,18 @@ const std::vector<char> DepositItemCommandDTO::serialize() const {
     byte_msg.resize(total_size);
 
     // Tipo de comando
-    byte_msg[0] = CMD_DEPOSIT_ITEM;
+    byte_msg[0] = CMD_WITHDRAW_GOLD;
 
     // Longitud de los argumentos
     byte_msg[1] = arguments_size;
 
     // Argumentos
-    uint16_t item_type = htons(itemType), pos_x = htons(bankerPosX),
+    uint16_t quant = htons(quantity), pos_x = htons(bankerPosX),
              pos_y = htons(bankerPosY);
 
-    memcpy(&byte_msg[2], &item_type, SIZE_8);
-    memcpy(&byte_msg[3], &pos_x, SIZE_16);
-    memcpy(&byte_msg[5], &pos_y, SIZE_16);
+    memcpy(&byte_msg[2], &quant, SIZE_16);
+    memcpy(&byte_msg[4], &pos_x, SIZE_16);
+    memcpy(&byte_msg[6], &pos_y, SIZE_16);
 
     return byte_msg;
 }
