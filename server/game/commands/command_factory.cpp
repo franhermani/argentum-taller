8fb603b6fa6097c5d1fdded9ec68122e118c9f66
franhermani <franhermani@gmail.com>
2020-07-05 03:49:34 -0300

    Added withdraw gold command

--- server/game/commands/command_factory.cpp
+++ server/game/commands/command_factory.cpp
@@ -10,6 +10,7 @@
 #include "deposit_item_command.h"
 #include "withdraw_item_command.h"
 #include "deposit_gold_command.h"
+#include "withdraw_gold_command.h"
 #include "list_command.h"
 #include "take_command.h"
 #include "throw_command.h"
@@ -71,7 +72,13 @@ Command* CommandFactory::operator()(Player& player, int type,
                 ntohs(pos_x), ntohs(pos_y));
 
     } else if (type == CMD_WITHDRAW_GOLD) {
-        // TODO:...
+        uint16_t quantity;
+        uint16_t pos_x, pos_y;
+        memcpy(&quantity, arguments.data(), SIZE_16);
+        memcpy(&pos_x, arguments.data() + SIZE_16, SIZE_16);
+        memcpy(&pos_y, arguments.data() + SIZE_16 + SIZE_16, SIZE_16);
+        return new WithdrawGoldCommand(player, quantity,
+                ntohs(pos_x), ntohs(pos_y));
 
     } else if (type == CMD_LIST) {
         uint16_t pos_x, pos_y;
--- server/game/commands/withdraw_gold_command.h
+++ server/game/commands/withdraw_gold_command.h
@@ -1,28 +1,28 @@
-#ifndef DEPOSIT_GOLD_COMMAND_H
-#define DEPOSIT_GOLD_COMMAND_H
+#ifndef WITHDRAW_GOLD_COMMAND_H
+#define WITHDRAW_GOLD_COMMAND_H
 
 #include "command.h"
 #include "../entities/living_beings/player.h"
 
-class DepositGoldCommand : public Command {
+class WithdrawGoldCommand : public Command {
     Player& player;
     uint16_t quantity;
     uint16_t bankerPosX, bankerPosY;
 
 public:
     // Constructor
-    DepositGoldCommand(Player& player, const uint16_t quantity,
+    WithdrawGoldCommand(Player& player, const uint16_t quantity,
     const uint16_t banker_pos_x, const uint16_t banker_pos_y);
 
     // Constructor y asignacion por copia deshabilitados
-    DepositGoldCommand(const DepositGoldCommand&) = delete;
-    DepositGoldCommand& operator=(const DepositGoldCommand&) = delete;
+    WithdrawGoldCommand(const WithdrawGoldCommand&) = delete;
+    WithdrawGoldCommand& operator=(const WithdrawGoldCommand&) = delete;
 
     // Destructor
-    ~DepositGoldCommand() override;
+    ~WithdrawGoldCommand() override;
 
     // Deposita una cantidad de oro en el banco
     void execute(World& world) override;
 };
 
-#endif // DEPOSIT_GOLD_COMMAND_H
+#endif // WITHDRAW_GOLD_COMMAND_H
