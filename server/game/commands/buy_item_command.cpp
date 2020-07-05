edb2ec6092a077ca18cd6f43f9bdb50d623539ab
franhermani <franhermani@gmail.com>
2020-07-05 17:16:09 -0300

    Added Buy Item Command, missing execute method

---
+++ server/game/commands/buy_item_command.cpp
@@ -0,0 +1,16 @@
+#include "buy_item_command.h"
+#include "../entities/npcs/banker.h"
+
+BuyItemCommand::BuyItemCommand(Player &player,
+        const uint8_t item_type, const uint16_t npc_pos_x,
+        const uint16_t npc_pos_y) :
+player(player), itemType(item_type),
+NPCPosX(npc_pos_x), NPCPosY(npc_pos_y) {}
+
+BuyItemCommand::~BuyItemCommand() = default;
+
+void BuyItemCommand::execute(World& world) {
+//    auto* banker = dynamic_cast<Banker*>
+//    (world.getNPCByPos(bankerPosX, bankerPosY));
+//    banker->withdrawItem(player, itemType);
+}
