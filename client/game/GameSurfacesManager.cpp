00ffbe5738f9cc984d9c57c19b96ee4c78243a1d
Martin <martin.rosas@despegar.com>
2020-07-06 01:34:40 -0300

    added surfaes manager

---
+++ client/game/GameSurfacesManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by martinrosas on 6/7/20.
+//
+
+#include "GameSurfacesManager.h"
---
+++ client/game/GameSurfacesManager.h
@@ -0,0 +1,14 @@
+//
+// Created by martinrosas on 6/7/20.
+//
+
+#ifndef ARGENTUM_GAMESURFACESMANAGER_H
+#define ARGENTUM_GAMESURFACESMANAGER_H
+
+
+class GameSurfacesManager {
+
+};
+
+
+#endif //ARGENTUM_GAMESURFACESMANAGER_H
