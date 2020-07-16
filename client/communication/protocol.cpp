3afee3ef62b6ef1bf14e634c5a3131eb81427e89
franhermani <franhermani@gmail.com>
2020-07-16 17:51:23 -0300

    Modularized client protocol receiveWorldUpdate

--- client/communication/protocol.cpp
+++ client/communication/protocol.cpp
@@ -1,13 +1,11 @@
 #include <string>
 #include <vector>
-#include <utility>
 #include <cstring>
 #include "protocol.h"
-#include "../../common/defines/debug.h"
 
-#define SIZE_8                      sizeof(uint8_t)
-#define SIZE_16                     sizeof(uint16_t)
-#define SIZE_32                     sizeof(uint32_t)
+#define SIZE_8          sizeof(uint8_t)
+#define SIZE_16         sizeof(uint16_t)
+#define SIZE_32         sizeof(uint32_t)
 
 ClientProtocol::ClientProtocol(Socket& socket) : socket(socket) {}
 
@@ -47,6 +45,7 @@ const int ClientProtocol::receiveUsernameConfirmation() {
 
     code.resize(message_length);
     socket.receiveBytes(code.data(), message_length);
+
     return (int) code[0];
 }
 
@@ -58,6 +57,7 @@ const int ClientProtocol::receiveUsernameId() {
     byte_msg.resize(message_length);
     socket.receiveBytes(byte_msg.data(), message_length);
     memcpy(&id, byte_msg.data(), message_length);
+
     return ntohs(id);
 }
 
@@ -194,50 +194,58 @@ world_t ClientProtocol::receiveWorldUpdate() {
     // ------------------------ //
     
     int pos = 0;
-    
-    // ---------------- //
-    // Player principal //
-    // ---------------- //
 
+    loadPlayerInfo(world_buffer, pos, w);
+    loadPlayers(world_buffer, pos, w);
+    loadCreatures(world_buffer, pos, w);
+    loadItems(world_buffer, pos, w);
+    loadGolds(world_buffer, pos, w);
+    loadAttacks(world_buffer, pos, w);
+
+    return w;
+}
+
+void ClientProtocol::loadPlayerInfo(std::vector<char>& buffer, int& pos,
+                                    world_t& w) {
     // Mana actual
     uint16_t actual_mana;
-    memcpy(&actual_mana, world_buffer.data() + pos, SIZE_16);
+    memcpy(&actual_mana, buffer.data() + pos, SIZE_16);
     w.player_info.actual_mana = ntohs(actual_mana);
     pos += SIZE_16;
 
     // Mana maxima
     uint16_t max_mana;
-    memcpy(&max_mana, world_buffer.data() + pos, SIZE_16);
+    memcpy(&max_mana, buffer.data() + pos, SIZE_16);
     w.player_info.max_mana = ntohs(max_mana);
     pos += SIZE_16;
 
     // Oro actual
     uint16_t actual_gold;
-    memcpy(&actual_gold, world_buffer.data() + pos, SIZE_16);
+    memcpy(&actual_gold, buffer.data() + pos, SIZE_16);
     w.player_info.actual_gold = ntohs(actual_gold);
     pos += SIZE_16;
 
     // Oro maximo
     uint16_t max_gold;
-    memcpy(&max_gold, world_buffer.data() + pos, SIZE_16);
+    memcpy(&max_gold, buffer.data() + pos, SIZE_16);
     w.player_info.max_gold = ntohs(max_gold);
     pos += SIZE_16;
 
     // Experiencia actual del nivel
     uint32_t level_actual_exp;
-    memcpy(&level_actual_exp, world_buffer.data() + pos, SIZE_32);
+    memcpy(&level_actual_exp, buffer.data() + pos, SIZE_32);
     w.player_info.level_actual_experience = ntohl(level_actual_exp);
     pos += SIZE_32;
 
     // Experiencia maxima del nivel
     uint32_t level_max_exp;
-    memcpy(&level_max_exp, world_buffer.data() + pos, SIZE_32);
+    memcpy(&level_max_exp, buffer.data() + pos, SIZE_32);
     w.player_info.level_max_experience = ntohl(level_max_exp);
     pos += SIZE_32;
 
     // Inventario
     uint8_t inventory_length;
-    memcpy(&inventory_length, world_buffer.data() + pos, SIZE_8);
+    memcpy(&inventory_length, buffer.data() + pos, SIZE_8);
     w.player_info.inventory.length = inventory_length;
     w.player_info.inventory.items.resize(inventory_length);
     pos += SIZE_8;
@@ -245,18 +253,17 @@ world_t ClientProtocol::receiveWorldUpdate() {
     int i;
     for (i = 0; i < inventory_length; i ++) {
         uint8_t item_type;
-        memcpy(&item_type, world_buffer.data() + pos, SIZE_8);
+        memcpy(&item_type, buffer.data() + pos, SIZE_8);
         w.player_info.inventory.items[i] = item_type;
         pos += SIZE_8;
     }
+}
 
-    // ------- //
-    // Players //
-    // ------- //
-
+void ClientProtocol::loadPlayers(std::vector<char>& buffer, int& pos,
+                                 world_t& w) {
     // Cantidad de Players
     uint16_t num_players;
-    memcpy(&num_players, world_buffer.data() + pos, SIZE_16);
+    memcpy(&num_players, buffer.data() + pos, SIZE_16);
     w.num_players = ntohs(num_players);
     pos += SIZE_16;
 
@@ -264,104 +271,104 @@ world_t ClientProtocol::receiveWorldUpdate() {
     std::vector<player_t> players;
     players.resize(w.num_players * sizeof(player_t));
 
+    int i;
     for (i = 0; i < w.num_players; i ++) {
         player_t player;
 
         // Id
         uint16_t id;
-        memcpy(&id, world_buffer.data() + pos, SIZE_16);
+        memcpy(&id, buffer.data() + pos, SIZE_16);
         player.id = ntohs(id);
         pos += SIZE_16;
 
         // Pos x en la matriz
         uint16_t pos_x;
-        memcpy(&pos_x, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_x, buffer.data() + pos, SIZE_16);
         player.pos.x = ntohs(pos_x);
         pos += SIZE_16;
 
         // Pos y en la matriz
         uint16_t pos_y;
-        memcpy(&pos_y, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_y, buffer.data() + pos, SIZE_16);
         player.pos.y = ntohs(pos_y);
         pos += SIZE_16;
 
         // Vida actual
         uint16_t actual_life;
-        memcpy(&actual_life, world_buffer.data() + pos, SIZE_16);
+        memcpy(&actual_life, buffer.data() + pos, SIZE_16);
         player.actual_life = ntohs(actual_life);
         pos += SIZE_16;
 
         // Vida maxima
         uint16_t max_life;
-        memcpy(&max_life, world_buffer.data() + pos, SIZE_16);
+        memcpy(&max_life, buffer.data() + pos, SIZE_16);
         player.max_life = ntohs(max_life);
         pos += SIZE_16;
 
         // Nivel
         uint16_t level;
-        memcpy(&level, world_buffer.data() + pos, SIZE_16);
+        memcpy(&level, buffer.data() + pos, SIZE_16);
         player.level = ntohs(level);
         pos += SIZE_16;
 
         // Enum type de la orientacion
         uint8_t orientation;
-        memcpy(&orientation, world_buffer.data() + pos, SIZE_8);
+        memcpy(&orientation, buffer.data() + pos, SIZE_8);
         player.orientation = orientation;
         pos += SIZE_8;
 
         // Enum type del estado
         uint8_t state;
-        memcpy(&state, world_buffer.data() + pos, SIZE_8);
+        memcpy(&state, buffer.data() + pos, SIZE_8);
         player.state = state;
         pos += SIZE_8;
 
         // Enum type de la raza
         uint8_t race_type;
-        memcpy(&race_type, world_buffer.data() + pos, SIZE_8);
+        memcpy(&race_type, buffer.data() + pos, SIZE_8);
         player.race_type = race_type;
         pos += SIZE_8;
 
         // Enum type de la clase
         uint8_t class_type;
-        memcpy(&class_type, world_buffer.data() + pos, SIZE_8);
+        memcpy(&class_type, buffer.data() + pos, SIZE_8);
         player.class_type = class_type;
         pos += SIZE_8;
 
         // Enum type del arma
         uint8_t weapon;
-        memcpy(&weapon, world_buffer.data() + pos, SIZE_8);
+        memcpy(&weapon, buffer.data() + pos, SIZE_8);
         player.weapon = weapon;
         pos += SIZE_8;
 
         // Enum type de la armadura
         uint8_t armor;
-        memcpy(&armor, world_buffer.data() + pos, SIZE_8);
+        memcpy(&armor, buffer.data() + pos, SIZE_8);
         player.armor = armor;
         pos += SIZE_8;
 
         // Enum type del casco
         uint8_t helmet;
-        memcpy(&helmet, world_buffer.data() + pos, SIZE_8);
+        memcpy(&helmet, buffer.data() + pos, SIZE_8);
         player.helmet = helmet;
         pos += SIZE_8;
 
         // Enum type del escudo
         uint8_t shield;
-        memcpy(&shield, world_buffer.data() + pos, SIZE_8);
+        memcpy(&shield, buffer.data() + pos, SIZE_8);
         player.shield = shield;
         pos += SIZE_8;
 
         players[i] = player;
     }
     w.players = players;
+}
 
-    // --------- //
-    // Criaturas //
-    // --------- //
-
+void ClientProtocol::loadCreatures(std::vector<char>& buffer, int& pos,
+                                   world_t& w) {
     // Cantidad de Criaturas
     uint16_t num_creatures;
-    memcpy(&num_creatures, world_buffer.data() + pos, SIZE_16);
+    memcpy(&num_creatures, buffer.data() + pos, SIZE_16);
     w.num_creatures = ntohs(num_creatures);
     pos += SIZE_16;
 
@@ -369,68 +376,68 @@ world_t ClientProtocol::receiveWorldUpdate() {
     std::vector<creature_t> creatures;
     creatures.resize(w.num_creatures * sizeof(creature_t));
 
+    int i;
     for (i = 0; i < w.num_creatures; i ++) {
         creature_t creature;
 
         // Pos x en la matriz
         uint16_t pos_x;
-        memcpy(&pos_x, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_x, buffer.data() + pos, SIZE_16);
         creature.pos.x = ntohs(pos_x);
         pos += SIZE_16;
 
         // Pos y en la matriz
         uint16_t pos_y;
-        memcpy(&pos_y, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_y, buffer.data() + pos, SIZE_16);
         creature.pos.y = ntohs(pos_y);
         pos += SIZE_16;
 
         // Vida actual
         uint16_t actual_life;
-        memcpy(&actual_life, world_buffer.data() + pos, SIZE_16);
+        memcpy(&actual_life, buffer.data() + pos, SIZE_16);
         creature.actual_life = ntohs(actual_life);
         pos += SIZE_16;
 
         // Vida maxima
         uint16_t max_life;
-        memcpy(&max_life, world_buffer.data() + pos, SIZE_16);
+        memcpy(&max_life, buffer.data() + pos, SIZE_16);
         creature.max_life = ntohs(max_life);
         pos += SIZE_16;
 
         // Nivel
         uint16_t level;
-        memcpy(&level, world_buffer.data() + pos, SIZE_16);
+        memcpy(&level, buffer.data() + pos, SIZE_16);
         creature.level = ntohs(level);
         pos += SIZE_16;
 
         // Enum type del tipo
         uint8_t type;
-        memcpy(&type, world_buffer.data() + pos, SIZE_8);
+        memcpy(&type, buffer.data() + pos, SIZE_8);
         creature.type = type;
         pos += SIZE_8;
 
         // Enum type de la orientacion
         uint8_t orientation;
-        memcpy(&orientation, world_buffer.data() + pos, SIZE_8);
+        memcpy(&orientation, buffer.data() + pos, SIZE_8);
         creature.orientation = orientation;
         pos += SIZE_8;
 
         // Enum type del estado
         uint8_t state;
-        memcpy(&state, world_buffer.data() + pos, SIZE_8);
+        memcpy(&state, buffer.data() + pos, SIZE_8);
         creature.state = state;
         pos += SIZE_8;
 
         creatures[i] = creature;
     }
     w.creatures = creatures;
+}
 
-    // ----- //
-    // Items //
-    // ----- //
-
+void ClientProtocol::loadItems(std::vector<char>& buffer, int& pos,
+                               world_t& w) {
     // Cantidad de Items
     uint16_t num_items;
-    memcpy(&num_items, world_buffer.data() + pos, SIZE_16);
+    memcpy(&num_items, buffer.data() + pos, SIZE_16);
     w.num_items = ntohs(num_items);
     pos += SIZE_16;
 
@@ -438,38 +445,38 @@ world_t ClientProtocol::receiveWorldUpdate() {
     std::vector<item_t> items;
     items.resize(w.num_items * sizeof(item_t));
 
+    int i;
     for (i = 0; i < w.num_items; i ++) {
         item_t item;
 
         // Pos x en la matriz
         uint16_t pos_x;
-        memcpy(&pos_x, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_x, buffer.data() + pos, SIZE_16);
         item.pos.x = ntohs(pos_x);
         pos += SIZE_16;
 
         // Pos y en la matriz
         uint16_t pos_y;
-        memcpy(&pos_y, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_y, buffer.data() + pos, SIZE_16);
         item.pos.y = ntohs(pos_y);
         pos += SIZE_16;
 
         // Enum type del item
         uint8_t item_type;
-        memcpy(&item_type, world_buffer.data() + pos, SIZE_8);
+        memcpy(&item_type, buffer.data() + pos, SIZE_8);
         item.type = item_type;
         pos += SIZE_8;
 
         items[i] = item;
     }
     w.items = items;
+}
 
-    // ---- //
-    // Oros //
-    // ---- //
-
+void ClientProtocol::loadGolds(std::vector<char>& buffer, int& pos,
+                               world_t& w) {
     // Cantidad de Oros
     uint16_t num_golds;
-    memcpy(&num_golds, world_buffer.data() + pos, SIZE_16);
+    memcpy(&num_golds, buffer.data() + pos, SIZE_16);
     w.num_golds = ntohs(num_golds);
     pos += SIZE_16;
 
@@ -477,38 +484,38 @@ world_t ClientProtocol::receiveWorldUpdate() {
     std::vector<gold_t> golds;
     golds.resize(w.num_golds * sizeof(gold_t));
 
+    int i;
     for (i = 0; i < w.num_golds; i ++) {
         gold_t gold;
 
         // Pos x en la matriz
         uint16_t pos_x;
-        memcpy(&pos_x, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_x, buffer.data() + pos, SIZE_16);
         gold.pos.x = ntohs(pos_x);
         pos += SIZE_16;
 
         // Pos y en la matriz
         uint16_t pos_y;
-        memcpy(&pos_y, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_y, buffer.data() + pos, SIZE_16);
         gold.pos.y = ntohs(pos_y);
         pos += SIZE_16;
 
         // Cantidad
         uint16_t quantity;
-        memcpy(&quantity, world_buffer.data() + pos, SIZE_16);
+        memcpy(&quantity, buffer.data() + pos, SIZE_16);
         gold.quantity = ntohs(quantity);
         pos += SIZE_16;
 
         golds[i] = gold;
     }
     w.golds = golds;
+}
 
-    // ------- //
-    // Ataques //
-    // ------- //
-
+void ClientProtocol::loadAttacks(std::vector<char>& buffer, int& pos,
+                                 world_t& w) {
     // Cantidad de Ataques
     uint16_t num_attacks;
-    memcpy(&num_attacks, world_buffer.data() + pos, SIZE_16);
+    memcpy(&num_attacks, buffer.data() + pos, SIZE_16);
     w.num_attacks = ntohs(num_attacks);
     pos += SIZE_16;
 
@@ -516,58 +523,65 @@ world_t ClientProtocol::receiveWorldUpdate() {
     std::vector<attack_t> attacks;
     attacks.resize(w.num_attacks * sizeof(attack_t));
 
+    int i;
     for (i = 0; i < w.num_attacks; i ++) {
         attack_t attack;
 
         // Pos x en la matriz
         uint16_t pos_x;
-        memcpy(&pos_x, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_x, buffer.data() + pos, SIZE_16);
         attack.pos.x = ntohs(pos_x);
         pos += SIZE_16;
 
         // Pos y en la matriz
         uint16_t pos_y;
-        memcpy(&pos_y, world_buffer.data() + pos, SIZE_16);
+        memcpy(&pos_y, buffer.data() + pos, SIZE_16);
         attack.pos.y = ntohs(pos_y);
         pos += SIZE_16;
 
         // Enum type de la orientacion
         uint8_t orientation;
-        memcpy(&orientation, world_buffer.data() + pos, SIZE_8);
+        memcpy(&orientation, buffer.data() + pos, SIZE_8);
         attack.orientation = orientation;
         pos += SIZE_8;
 
         // Enum type del tipo
         uint8_t type;
-        memcpy(&type, world_buffer.data() + pos, SIZE_8);
+        memcpy(&type, buffer.data() + pos, SIZE_8);
         attack.type = type;
         pos += SIZE_8;
 
         // Enum type del sonido
         uint8_t sound;
-        memcpy(&sound, world_buffer.data() + pos, SIZE_8);
+        memcpy(&sound, buffer.data() + pos, SIZE_8);
         attack.sound = sound;
         pos += SIZE_8;
 
         // 1 si esta colisionando, 0 si no
         uint8_t is_colliding;
-        memcpy(&is_colliding, world_buffer.data() + pos, SIZE_8);
+        memcpy(&is_colliding, buffer.data() + pos, SIZE_8);
         attack.is_colliding = is_colliding;
         pos += SIZE_8;
 
         attacks[i] = attack;
     }
     w.attacks = attacks;
-
-    return w;
 }
 
 list_t ClientProtocol::receiveItemsList() {
     list_t list;
+
+    // --------------------- //
+    // Recepcion del mensaje //
+    // --------------------- //
+
     int static_part_size = SIZE_16 + SIZE_16 + SIZE_8;
     std::vector<char> item_static_buffer(static_part_size, 0);
     socket.receiveBytes(item_static_buffer.data(), item_static_buffer.size());
 
+    // ----------------------- //
+    // Carga del struct list_t //
+    // ----------------------- //
     int pos = 0;
 
     uint8_t show_price;
