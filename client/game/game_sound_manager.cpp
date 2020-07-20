8289ecb580c0acbc4645e92e17bbf41baa1031a2
Martin <martin.rosas@despegar.com>
2020-07-20 19:57:38 -0300

    added arrow and heal strik

--- client/game/game_sound_manager.cpp
+++ client/game/game_sound_manager.cpp
@@ -39,12 +39,15 @@ void GameSoundManager::initSounds() {
     std::string path_explosion = "../client/resources/audio/explosion.wav";
     std::string path_melee = "../client/resources/audio/melee.wav";
     std::string path_magic_strike = "../client/resources/audio/shot.mp3";
-    std::string arrow_strike = "../client/resources/audio/arrow.mp3";
+    std::string path_arrow_strike = "../client/resources/audio/arrow.mp3";
+    std::string path_heal = "../client/resources/audio/flute.wav";
     addNewSound(path_melee, PLAYER_PUNCH);
     addNewSound(path_melee, CREATURE_PUNCH);
+    addNewSound(path_arrow_strike, ARROW_STRIKE);
     addNewSound(path_sword, SWORD_STRIKE);
     addNewSound(path_magic_strike, MAGIC_STRIKE);
     addNewSound(path_explosion, EXPLOSION);
+    addNewSound(path_heal, HEAL_STRIKE);
     soundOn = true;
 }
 
